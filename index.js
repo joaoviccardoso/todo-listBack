0e7c1d2960274f9671c454cc60a57aafb15d0e86
seu nome de usuário <pealq63@gmail.com>
2025-05-28 16:00:30 -0300

    adicionado cors na pagina para conecatar com uma pagina front end

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 import express from "express";
+import cors from "cors";
 import db from "./config/connectDb.js";
 import router from "./routes/routesTarefas.js";
 import manipuladorDeErro from "./maddleweres/manipuladorDeErros.js";
 import manipulador404 from "./maddleweres/manipulador404.js";
 
-const app = express();
 
+const app = express(); // <=== DEVE vir antes do uso de app.use
+
+// Configurações
+app.use(cors({
+  origin: 'http://127.0.0.1:5500'
+}));
 app.use(express.json());
 
 db.on("erro", console.log.bind(console, 'Erro de conexão'))
@@ -16,6 +22,5 @@ db.once("open", () => {
 app.use("/", router);
 app.use(manipulador404)
 app.use(manipuladorDeErro)
-
 
 app.listen(3000)
