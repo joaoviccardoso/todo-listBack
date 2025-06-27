285a20b24de9413dac90afe63200cc5a5d988686
seu nome de usuário <pealq63@gmail.com>
2025-06-27 16:14:50 -0300

    adicionado configuração para hospedar o back end no render

--- index.js
+++ index.js
@@ -23,4 +23,8 @@ app.use("/", router);
 app.use(manipulador404)
 app.use(manipuladorDeErro)
 
-app.listen(3000)
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
